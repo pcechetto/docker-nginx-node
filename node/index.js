f1a494235c5576e9b90bb72e20aae4ddb2ca6876
Pedro Cechetto <pedrocechetto@hotmail.com>
2023-09-18 15:48:09 -0300

    find all

--- node/index.js
+++ node/index.js
@@ -12,12 +12,45 @@ const connection = mysql.createConnection(config)
 
 const sql = `INSERT INTO people(name) values('pedro')`
 connection.query(sql)
-connection.end()
+
+function findAll(callback) {
+    const sql = 'SELECT * FROM people';
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, results);
+        }
+    });
+}
+
+findAll((err, results) => {
+    if (err) {
+        console.error('Erro ao buscar registros:', err);
+    } else {
+        console.log('Registros encontrados:', results);
+    }
+
+    // Fecha a conexão com o banco de dados após a consulta
+
+});
 
 app.get("/", (req, res) => {
-    res.send('<h1>pedro informatica</h1>')
-})
+    findAll((err, results) => {
+        if (err) {
+            console.error('Erro ao buscar registros:', err);
+            res.status(500).send('Erro ao buscar registros');
+        } else {
+            console.log('Registros encontrados:', results);
+            res.send('<h1>Full Cycle Rocks!</h1>' + JSON.stringify(results));
+        }
+    });
+});
+
+
 
 app.listen(port, ()=> {
     console.log("running on port " + port)
+
 })
