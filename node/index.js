6e9b8178704d75a538362f9b62ca37a62b4ed3a6
Pedro Cechetto <pedrocechetto@hotmail.com>
2023-09-18 15:00:02 -0300

    db conn

--- node/index.js
+++ node/index.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 8080
-// const config = {
-//     host: 'db',
-//     user: 'root',
-//     password: 'root',
-//     database: 'nodedb'
-// };
-// const mysql = require('mysql')
-// const connection = mysql.createConnection(config)
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database:'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
 
-// const sql = `INSERT INTO people(name) values('pedro')`
-// connection.query(sql)
-// connection.end()
+const sql = `INSERT INTO people(name) values('pedro')`
+connection.query(sql)
+connection.end()
 
 app.get("/", (req, res) => {
     res.send('<h1>pedro informatica</h1>')
 })
 
 app.listen(port, ()=> {
-    console.log("running on port" + port)
+    console.log("running on port " + port)
 })
